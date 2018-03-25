c886b16bba68bb8ad9ae78e7c71c6e687f17f8c4
Halton Huo <halton.huo@gmail.com>
2018-03-25 18:43:30 +0800

    Implement division of single number.

--- src/components/DivisionWithRemain.vue
+++ src/components/DivisionWithRemain.vue
@@ -1,18 +1,61 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
+  <div class="divisionWithRemain">
+    <h1>{{ title }}</h1>
+    <ul id="example-1">
+      <li v-for="i in exercises" :key="i.id">
+        {{ i.division }} &divide; {{ i.divider}} = &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+      </li>
+    </ul>
+    <button v-on:click="refresh">更新题库</button>
+    <button v-on:click="print">打印</button>
   </div>
 </template>
 
 <script>
+
+function randomInRange(min, max) {
+  return Math.floor((Math.random() * ((max - min) + 1)) + min);
+}
+
+function generateExercise() {
+  // Get a dividend in range [20, 90]
+  const mult1 = randomInRange(2, 9);
+  const mult2 = randomInRange(2, 9);
+  const remain = randomInRange(2, mult2);
+  const total = (mult1 * mult2) + remain;
+
+  return {
+    division: total,
+    divider: mult1,
+  };
+}
+
 export default {
   name: 'DivisionWithRemain',
+  mounted: function foo() {
+    this.refresh();
+  },
   data() {
     return {
-      msg: '100以内除法带余数',
+      title: '一位数除法带余数',
+      exercises: [
+      ],
     };
   },
+  methods: {
+    refresh: function refresh() {
+      let count = 0;
+      while (count < 100) {
+        this.exercises.push(generateExercise());
+        count += 1;
+      }
+    },
+    print: function print() {
+      window.print();
+    },
+  },
 };
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
