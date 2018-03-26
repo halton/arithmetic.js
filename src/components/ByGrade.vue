9a2c983477c263902eac6eed3d0f2bddb9b29301
Halton Huo <halton.huo@gmail.com>
2018-03-26 16:46:07 +0800

    Move code into ByGrade.

---
+++ src/components/ByGrade.vue
@@ -0,0 +1,70 @@
+<template>
+  <div>
+    <template v-for="grade in grades">
+     <v-expansion-panel>
+      <v-expansion-panel-content>
+        <div slot="header"><v-icon>{{ grade.icon }}</v-icon> {{ grade.text }}</div>
+        <v-list dense v-if="grade.exams.length">
+          <template v-for="exam in grade.exams">
+            <v-list-tile @click="">
+              <v-list-tile-action>
+                <v-icon>{{ exam.icon }}</v-icon>
+              </v-list-tile-action>
+              <v-list-tile-content>
+                <v-list-tile-title>{{ exam.text }}</v-list-tile-title>
+              </v-list-tile-content>
+            </v-list-tile>
+          </template>
+        </v-list>
+      </v-expansion-panel-content>
+    </v-expansion-panel>
+    </template>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ByGrade',
+  data: () => ({
+    grades: [
+      {
+        icon: 'looks_one',
+        text: '一年级',
+        expander: true,
+        exams: [
+          { icon: 'folder', text: '100以内加法' },
+          { icon: 'folder', text: '100以内减法' },
+        ],
+      },
+      {
+        icon: 'looks_two',
+        text: '二年级',
+        exams: [
+          { icon: 'folder', text: '一位数除法带余数' },
+        ],
+      },
+      {
+        icon: 'looks_3',
+        text: '三年级',
+        exams: [],
+      },
+      {
+        icon: 'looks_4',
+        text: '四年级',
+        exams: [],
+      },
+      {
+        icon: 'looks_5',
+        text: '五年级',
+        exams: [],
+      },
+      {
+        icon: 'looks_6',
+        text: '六年级',
+        exams: [],
+      },
+
+    ],
+  }),
+};
+</script>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -7,64 +7,7 @@
         v-model="drawer"
       >
       <v-list dense>
-        <v-expansion-panel>
-          <v-expansion-panel-content>
-            <div slot="header"><v-icon>looks_one</v-icon>一年级</div>
-            <v-list dense>
-              <v-list-tile @click="">
-                <v-list-tile-action>
-                  <v-icon>folder</v-icon>
-                </v-list-tile-action>
-                <v-list-tile-content>
-                  <v-list-tile-title>100以内加法</v-list-tile-title>
-                </v-list-tile-content>
-              </v-list-tile>
-              <v-list-tile @click="">
-                <v-list-tile-action>
-                  <v-icon>folder</v-icon>
-                </v-list-tile-action>
-                <v-list-tile-content>
-                  <v-list-tile-title>100以内减法</v-list-tile-title>
-                </v-list-tile-content>
-              </v-list-tile>
-            </v-list>
-          </v-expansion-panel-content>
-        </v-expansion-panel>
-        <v-expansion-panel>
-          <v-expansion-panel-content>
-            <div slot="header"><v-icon>looks_two</v-icon>二年级</div>
-            <v-list dense>
-              <v-list-tile @click="">
-                <v-list-tile-action>
-                  <v-icon>folder</v-icon>
-                </v-list-tile-action>
-                <v-list-tile-content>
-                  <v-list-tile-title>一位数除法带余数</v-list-tile-title>
-                </v-list-tile-content>
-              </v-list-tile>
-            </v-list>
-          </v-expansion-panel-content>
-        </v-expansion-panel>
-        <v-expansion-panel>
-          <v-expansion-panel-content>
-            <div slot="header"><v-icon>looks_3</v-icon>三年级</div>
-          </v-expansion-panel-content>
-        </v-expansion-panel>
-        <v-expansion-panel>
-          <v-expansion-panel-content>
-            <div slot="header"><v-icon>looks_4</v-icon>四年级</div>
-          </v-expansion-panel-content>
-        </v-expansion-panel>
-        <v-expansion-panel>
-          <v-expansion-panel-content>
-            <div slot="header"><v-icon>looks_5</v-icon>五年级</div>
-          </v-expansion-panel-content>
-        </v-expansion-panel>
-        <v-expansion-panel>
-          <v-expansion-panel-content>
-            <div slot="header"><v-icon>looks_6</v-icon>六年级</div>
-          </v-expansion-panel-content>
-        </v-expansion-panel>
+        <by-grade></by-grade>
         <v-list-tile @click="">
           <v-list-tile-action>
             <v-icon>backup</v-icon>
@@ -112,12 +55,17 @@
 </template>
 
 <script>
+import ByGrade from '@/components/ByGrade';
+
 export default {
   data: () => ({
     drawer: null,
   }),
   props: {
     source: String,
+  },
+  components: {
+    'by-grade': ByGrade,
   },
 };
 </script>
